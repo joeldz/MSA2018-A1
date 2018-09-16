0d9fd868c351801c0a4839e556b308109a67ae19
Joel De Zoysa <joel.de.zoysa@gmail.com>
2018-09-16 14:40:39 +1200

    Can call API with user desired input

--- jdez-app/src/App.tsx
+++ jdez-app/src/App.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react';
-import Form from "./components/Form";
 import Price from "./components/Price";
 import Title from "./components/Title";
 import './App.css';
 
 class App extends React.Component {
+  getPrice = async (e:any) => {
+    e.preventDefault();
+    const symbol = e.target.elements.symbol.value.toUpperCase();
+    const api_call = await fetch('https://min-api.cryptocompare.com/data/price?fsym='+symbol+'&tsyms=NZD,USD');
+    const data = await api_call.json();
+    console.log(data);
+  }
+  
   public render() {
     return (
       <div>
         <Title />
-        <Form />
+        <form onSubmit={this.getPrice}>
+          <input type="text" name="symbol" placeholder="Symbol..."/>
+          <button>Get Price</button>
+        </form>
         <Price />
       </div>
     );
